0e086e52e1e3db7c9772f5ed96fd6c92f9fa0311
Daniela Aguilar Ceballos <daaguilar@sistecredito.com>
2022-07-11 09:04:13 -0500

    modificando

--- CuotaVariable/cuotaVariable.js
+++ CuotaVariable/cuotaVariable.js
@@ -40,26 +40,3 @@ let calcularCuota = () => {
         cuotaVariable(monto,tasa,plazo);
     }
 }
-
-
-
-
-
-
-
-
-
-
-// var amortizacion = monto / plazo;
-
-// const formateador = new Intl.NumberFormat("en", { style: "currency", "currency": "COP" });
-
-// document.write(" Período  Interés  Amortización  Cuota  Saldo <br>");
-
-// for (let i = 1; i <= plazo; i++) {
-//     monto;
-//     var interes = monto * (tasa / 100);
-//     var cuota = amortizacion + interes;
-//     monto -= amortizacion;
-//     document.write(i + "---" + interes.toFixed(2) + "---" + amortizacion.toFixed(2) + "---" + cuota.toFixed(2) + "---" + monto.toFixed(2) + "<br>");
-// }
